473eee72836353cf44fa6f0e54d7eb7cea89be5a
Darren Wang <rardenawng@gmail.com>
2018-12-03 17:44:51 -0500

    Create sunflower_flask_app.py

---
+++ sunflower_flask_app.py
@@ -0,0 +1,73 @@
+from flask import Flask, render_template, request
+from validate_email import validate_email
+import MySQLdb
+import hashlib
+
+
+app = Flask(__name__)
+
+#Conenct into MySQL database
+cnx = MySQLdb.connect(host = "localhost",
+                          user = "root",
+                          passwd = "",
+                          db = "XXXX")
+                          
+#Crusor item to initiate text into MySQL
+cur = cnx.cursor()
+
+@app.route('/', methods=['GET','POST'])
+def login_page():
+
+    #Comparing input and items in MySQL
+    if request.method == 'POST':
+
+        #Stores input email address into String
+        inp_usrn = request.form['email address']
+
+        #Comparing email addresses
+        if db_has(inp_usrn):
+
+            #Stores input password into String
+            inp_pswd = request.form['password']
+
+            #Hashes input password using SHA224
+            hash_obj = hashlib.sha224(inp_pswd).hexdigest()
+
+            #Extract the password from MySQL
+            cur.execute("SELECT pswd FROM users WHERE email = '%s'" %(inp_usrn))
+            get_pswd = ""
+            for row in cur:
+                get_pswd += row[0]
+
+            #Hexdigest the has object and compares it to database's password
+            if hash_obj == get_pswd:
+                return render_template("main.html", user = inp_usrn, admin = ("example@example.com"==inp_usrn))
+    return render_template("login.html")
+
+@app.route('/signup',methods=['GET','POST'])
+def sign_up():
+
+    if request.method == 'POST':
+        
+        #Get password from the forms in createaccount.html using Flask request
+        create_usr = request.form['email address']
+        create_pswd = request.form['password']
+        
+        #Creating account and checking if email address is valid and has a SMTP Server
+        if not db_has(create_usr) and validate_email(create_usr,verify=True):
+            cur.execute("INSERT INTO `users` (`email`,`pswd`) VALUES ('%s', SHA2('%s',224));" %(create_usr,create_pswd))
+            cnx.commit()
+            return render_template("createaccount.html", text = "Account Created!")
+
+    return render_template("createaccount.html")
+
+#Checks if input emailaddress is in the database
+def db_has(ea):
+    #Extracts the username from MySQL (if applicable)
+    cur.execute("SELECT email FROM users WHERE email = '%s'" %(ea))
+    get_usrn = ""
+    for row in cur:
+        get_usrn += row[0]
+    if get_usrn == ea:
+        return True
+    return False
